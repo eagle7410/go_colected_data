6691450d69da5fa526d5994e2851faf7499baf26
Igor Stcherbina <orel1939@mail.ru>
2016-07-09 00:55:24 +0300

    Start add static files

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"github.com/gin-gonic/contrib/renders/multitemplate"
+//    "github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
-//	"unicode/utf8"
+//	"html/template"
 	"io/ioutil"
+	"net/http"
 	"strconv"
 	"strings"
 	"bytes"
@@ -20,8 +23,10 @@ type (
 		Name, Login, Pass, Answer, Othen string
 	}
 	Data struct {
-		pass string
-		Data []Record
+		pass     string
+		passCode []int
+		Data     map[int]Record
+		Count    int
 	}
 )
 
@@ -31,34 +36,79 @@ var (
 	OK   bool
 )
 
-func pass (val *string) string {
+func toPass (val *string) (string, []int) {
 	s := ""
+	c := []int{}
 	buf := strings.Split(*val, "#")
-//	buffer := []byte
 
 	for i, _ := range buf {
 
 		n, _ := strconv.Atoi(buf[i])
 		if n > 0 {
-			n /= 2;
-			c := fmt.Sprintf("%q", n)
-			s += c
+			n /= 2
+			c = append(c, n)
+			char := fmt.Sprintf("%q", n)
+			s += char
 		}
 
+	}
+
+	return s, c
+}
+
+func (ins *Data) toWord (val *string) string {
+	s:= ""
+	passCount := 0
+	buf := strings.Split(*val, "#")
+
+	for i, _ := range buf {
+
+		if	passCount == len(ins.passCode) {
+			passCount = 0
+		}
+
+		n, _ := strconv.Atoi(buf[i])
+
+		if n > 0 {
+			n -= ins.passCode[passCount]
+			char := fmt.Sprintf("%q", n)
+			s += char
+		}
+
+		passCount++
 	}
 
 	return s
 }
 
 func (ins *Data) init (data *[]string) {
-
+	var count byte
+	rec := Record{}
+	ins.Data = make(map[int]Record)
 	for inx, val := range *data {
 
 		if inx == 0 {
 
-			ins.pass = pass(&val)
+			ins.pass, ins.passCode = toPass(&val)
 
 		} else {
+			count++
+			word := val
+
+			if	count == 1 {
+				rec.Name  = ins.toWord(&word)
+			} else if count == 2 {
+				rec.Login = ins.toWord(&word)
+			} else if count == 3 {
+				rec.Pass  = ins.toWord(&word)
+			} else if count == 4 {
+				rec.Answer  = ins.toWord(&word)
+			} else if count == 5 {
+				rec.Othen  = ins.toWord(&word)
+				count = 0
+				ins.Data[ins.Count] = rec
+				ins.Count++
+			}
 
 		}
 
@@ -92,8 +142,6 @@ func DataInit() {
 	data = Data{}
 	data.init(&dataArr)
 
-
-
 }
 
 func init() {
@@ -101,22 +149,41 @@ func init() {
 	DataInit()
 }
 
+func Render() {
+
+}
+
 func main() {
 
-	fmt.Println("data", data)
+//	fmt.Println("data", data)
 
 	if !OK {
 		return
 	}
 
-	r := gin.Default()
+	r := gin.New()
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(401, gin.H{
-			"Data": "GG",
-		})
+	// Static
+	// render
+	r.LoadHTMLGlob("tpl/*")
+//	r.HTMLRender = createMyRender()
+
+
+	r.GET("/", func (c *gin.Context) {
+//		r.HTMLTemplates = template.Must(template.ParseFiles("templates/layout.html", "templates/home.html"))
+		c.HTML(200, "index.html", gin.H{})
 
 	})
 
 	r.Run(":" + p.port) // listen and server on 0.0.0.0:8080
+}
+
+func createMyRender() multitemplate.Render {
+	r := multitemplate.New()
+	r.AddFromFiles("index", "base.html", "base.html")
+	r.AddFromFiles("article", "base.html", "article.html")
+	r.AddFromFiles("login", "base.html", "login.html")
+	r.AddFromFiles("dashboard", "base.html", "dashboard.html")
+
+	return r
 }
